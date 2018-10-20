b4bdabcc615cd8f498bbf1806c7e0aafd9fd7452
Dania A <dania@danias-macbook-pro.local>
2018-10-20 22:24:06 +0300

    Initial Commit

---
+++ Session 3 Activity/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Session 3 Activity
+//
+//  Created by Dania A on 02/09/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    // TODO: (1) Link this ViewController file with the login ViewController in the Storyboard
+    // TODO: (2) Set up all the views and constraints of the first and the second ViewControllers properly
+    // TODO: (3) Add a Navigation Controller and set up the first ViewController to be the root ViewController.
+    // TODO: (4) When the user clicks on Go to "login/ signup screen" button the login ViewController should appear and there should be a navigation bar with back button at the top.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
