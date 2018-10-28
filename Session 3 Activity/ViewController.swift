4bac0eeab336c819624064361db816dc016a2073
Hamada Ahmed <info@scarab-dev.com>
2018-10-28 21:15:27 +0300

    Fix Required TODOs :)

--- Session 3 Activity/ViewController.swift
+++ Session 3 Activity/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "goToLogin", sender: nil)
+        
     }
+    
 
 
 }
